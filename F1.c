58853a075aa46df9fce474f1ef05b7a5355092c6
devdasEV <163447951+devdasEV@users.noreply.github.com>
2024-03-15 01:29:30 +0700

    F1

--- F1.c
+++ F1.c
@@ -1,10 +1,1 @@
 #include <stdio.h>
-
-int main() {
-    char name[100];
-    printf("Enter your name (Eng) > ");
-    fgets(name, sizeof(name), stdin);
-    printf("Your name is %s", name);
-
-    return 0;
-}
